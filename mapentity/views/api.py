66063754817d6e8deef1e1a3c6e01853a8523a0c
J-E Castagnede <jean-etienne.castagnede@makina-corpus.com>
2016-03-11 09:45:24 +0100

    fix auto geom field with settings

--- mapentity/views/api.py
+++ mapentity/views/api.py
@@ -14,6 +14,7 @@ from .. import serializers as mapentity_serializers
 from rest_framework_gis.fields import GeometrySerializerMethodField
 from .base import BaseListView
 from .mixins import FilterListMixin, ModelViewMixin, JSONResponseMixin
+from mapentity.settings import app_settings
 
 
 logger = logging.getLogger(__name__)
@@ -74,16 +75,23 @@ class MapEntityViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         serializer = super(MapEntityViewSet, self).get_serializer_class()
         renderer, media_type = self.perform_content_negotiation(self.request)
-
+        
         if getattr(renderer, 'format') == 'geojson':
+            
             class Serializer(serializer, GeoFeatureModelSerializer):
-                geom = GeometrySerializerMethodField()
+                def __init__(self, *args, **kwargs):
+                    super(Serializer, self).__init__(*args, **kwargs)
+                    method_name = 'get_geom'
+                    
+                    if app_settings['GEOM_FIELD_NAME'] == 'geom':
+                        # dont specify method_name when field == geom, get_geom is default and redundant
+                        method_name = None
+                    
+                    self.fields[app_settings['GEOM_FIELD_NAME']] = GeometrySerializerMethodField(method_name)
 
                 def get_geom(self, obj):
-                    if hasattr(obj, 'geom'):
-                        return obj.geom
-                    else:
-                        return None
-
+                    return getattr(obj, app_settings['GEOM_FIELD_NAME'], None)
+                
             return Serializer
+        
         return serializer
